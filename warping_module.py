4cf50a8f6999de1cb1e7a8e77057a620251497d6
Nikita Chauhan <50770098+nikitach5@users.noreply.github.com>
2021-06-26 21:47:40 +0530

    Update warping_module.py

--- warping_module.py
+++ warping_module.py
@@ -41,4 +41,47 @@ def warping_module(volume, masks, transform):
     res = tf.reduce_max(res, reduction_indices=[-2])
 
     return res, arr
+"""
+def rotation_estimation(joint1i,joint2i,joint1f,joint2f,mask):
+  
+  a=joint1i-joint2i
+  b=joint1f-joint2f
+  
+  
+  scle=np.linalg.norm([b[0],b[1]])/np.linalg.norm([a[0],a[1]])
+  
+  angle=tf.math.atan(b[1]/b[0])-tf.math.atan(a[1]/a[0])
+  si=tf.math.sin(angle)
+  co=tf.math.cos(angle)
+  
+  rotation_mat=np.array([[co,-si],[si,co]])
+  
+  mxy=np.array([mask[0,:],mask[1,:]])
+  j2i=np.array([[joint2i[0]],[joint2i[1]]])
+  j2f=np.array([[joint2f[0]],[joint2f[1]]])
+  
+  xymask=np.matmul(rotation_mat,mxy-j2i)*scle+j2f
+  
+  
+  zmask=((xymask[0,:]-joint2f[0])*b[2]/b[0])+joint2f[2]
+  
+  zmask=np.reshape(zmask,(1,-1))
+  maskf=np.concatenate((xymask,zmask),axis=0)
+  
+  return maskf
+
+def warpingModule(mask,transform,joint):
+    warped_mask=[]
+    lefthand_upper  =   rotation_estimation(joint['lsho'],joint['lelb'],transform['lsho'],transform['lelb'],mask[' '])
+    righthand_upper =   rotation_estimation(joint['rsho'],joint['relb'],transform['rsho'],transform['relb'],mask[' '])
+    lefthand_lower  =   rotation_estimation(joint['lelb'],joint['lwri'],transform['lelb'],transform['lwri'],mask[' '])
+    righthand_lower =   rotation_estimation(joint['relb'],joint['rwri'],transform['relb'],transform['rwri'],mask[' '])
+    leftleg_upper   =   rotation_estimation(joint['lhip'],joint['lkne'],transform['lhip'],transform['lkne'],mask[' '])
+    rightleg_upper  =   rotation_estimation(joint['rhip'],joint['rkne'],transform['rhip'],transform['rkne'],mask[' '])
+    leftleg_lower   =   rotation_estimation(joint['lkne'],joint['lank'],transform['lkne'],transform['lank'],mask[' '])
+    rightleg_lower  =   rotation_estimation(joint['rkne'],joint['rank'],transform['rkne'],transform['rank'],mask[' '])
+    head            =   rotation_estimation(joint['lear'],joint['rear'],transform['lear'],transform['rear'],mask[' '])
+    body            =   rotation_estimation(joint['neck'],joint['pelv'],transform['neck'],transform['pelv'],mask[' '])
+
+"""
   
