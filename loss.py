41bd677d271a1d6ef8097b049d3ce37bf92a47b4
Fullstackmystic <80418551+Fullstackmystic@users.noreply.github.com>
2021-06-27 17:41:34 +0530

    Add files via upload

--- loss.py
+++ loss.py
@@ -42,3 +42,21 @@ def Discriminator():
                                 kernel_initializer=initializer)(zero_pad2) # (bs, 30, 30, 1)
 
   return keras.Model(inputs=ini, outputs=last)
+
+
+def gen_opt():
+  gstep = tf.train.get_or_create_global_step()
+  base_lr = generator_lr
+  # Halve the learning rate at 1000 steps.
+  lr = tf.cond(gstep < 1000, lambda: base_lr, lambda: base_lr / 2.0)
+  return tf.train.AdamOptimizer(lr, 0.5)
+
+gan_estimator = tfgan.estimator.GANEstimator(
+    generator_fn=generator_g,
+    discriminator_fn=discriminator_y,
+    generator_loss_fn=tfgan.losses.wasserstein_generator_loss,
+    discriminator_loss_fn=tfgan.losses.wasserstein_discriminator_loss,
+    params={'batch_size': train_batch_size, 'noise_dims': noise_dimensions},
+    generator_optimizer=gen_opt,
+    discriminator_optimizer=tf.compat.v1.train.AdamOptimizer(discriminator_lr, 0.5)
+    )
